eeaaee0b6e815fd6ddaf49045a7ada4bcf6af56d
unknown <jeromesus026@gmail.com>
2021-10-26 23:34:35 +0800

    2nd

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,8 @@
 const request = require('request')
 
-const url ='http://api.weatherstack.com/current?access_key=06258cf566e3f39de1d862a2c571cba9&query=37.8267,-122.4233'
+const url ='http://api.weatherstack.com/current?access_key=06258cf566e3f39de1d862a2c571cba9&query=37.8267,-122.4233&units=f'
 
-request({ url: url}, (error, response)=>{
-   const data = JSON.parse(response.body)
-   console.log(data.current)
+request({ url: url, json: true }, (error, response)=>{
+//    console.log(response.body.current)
+console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degress out. It feels like ' + response.body.current.feelslike + ' degress out.')
 })
