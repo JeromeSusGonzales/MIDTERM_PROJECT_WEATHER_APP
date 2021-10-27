603711d5652ab976960dc9da360e406d3cf330aa
unknown <jeromesus026@gmail.com>
2021-10-27 13:17:51 +0800

    commit

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,32 +1,30 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-// const url ='http://api.weatherstack.com/current?access_key=06258cf566e3f39de1d862a2c571cba9&query=37.8267,-122.4233&units=f'
+const address = process.argv[2]
+if(!address) {
+    console.log('Please provide an address')
+}else{
+    geocode(address, (error, data) => {
 
-// request({ url: url, json: true }, (error, response)=>{
-//     if (error){
-//        console.log('Unable to connect to weather service')
-//     }else if(response.body.error) {
-//          console.log('Unable to find Location')
-//     }
+        if(error){
+            return console.log(error)
+        }
     
-//     else{
-//         console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degress out. It feels like ' + response.body.current.feelslike + ' degress out.')
-//     }
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    
+        if(error){
+            return console.log(error)
+        }
+    
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+    
+    
+}
+console.log(process.argv)
 
-// })
-
-//Geocoding
- const geocodeUrL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Manila.json?access_token=pk.eyJ1IjoiamVyb21lc3VzIiwiYSI6ImNrdjg5cjJhNTlzMzcycXE2cm9sODFmcDIifQ.GPSgWxSWKfimfyaI9f7yRQ&limit=1'
- request({url: geocodeUrL, json: true},(error,response) => {
-
-    if(error){
-        console.log('Unable to connect to location services!')
-    }else if(response.body.features.lenght === 0){
-        console.log('Unable to find location. Try another search.')
-    }else{
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
-        console.log(latitude, longitude)
-    }
-  
- })
+ 
