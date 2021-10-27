d93f7cff22f567df9fd8b5babf6ef6af007b77ab
unknown <jeromesus026@gmail.com>
2021-10-27 16:59:55 +0800

    commit

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,22 +3,31 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello Express!')
+    res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res) => {
   
-    res.send('Help Page')
+    res.send([{
+       name:'Jerome'
+    }, {
+        name:'Ikaw'
+    }])
 })
+
+
 
 app.get('/about', (req, res) => {
 
-    res.send('About')
+    res.send('<h1>About</h1>')
 })
 
 app.get('/weather', (req, res) => {
 
-    res.send('Your weather')
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Sorsogon'
+    })
 })
 
 app.listen(3000, () => {
