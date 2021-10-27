ca1941c09b6ce476963633387ce4799657fefca6
unknown <jeromesus026@gmail.com>
2021-10-27 13:59:30 +0800

    commit

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -2,18 +2,18 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
   const url = 'http://api.weatherstack.com/current?access_key=06258cf566e3f39de1d862a2c571cba9&query=/' + latitude + ',' + longitude + '&units=f'
 
-  request({url: url, json: true}, (error, response) => {
+  request({url: url, json: true}, (error, {body} = {}) => {
       if (error){
 
          callback('Unable to connect to weather service!', undefined)
 
-      }else if(response.body.error){
+      }else if(body.error){
 
         callback('Unable to find Location.', undefined)
           
       }else{
 
-        callback(undefined, response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degress out. It feels like ' + response.body.current.feelslike + ' degress out.')
+        callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out.')
 
       }
 
