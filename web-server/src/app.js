8b83683336e900fcbd85400a8131adc9f9981860
unknown <jeromesus026@gmail.com>
2021-10-28 22:00:05 +0800

    commit

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -40,10 +40,28 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
-
+    if(!req.query.address){
+        return res.send({
+           error: 'You must provide an address!' 
+        })
+    }
     res.send({
         forecast: 'It is snowing',
-        location: 'Sorsogon'
+        location: 'Sorsogon',
+        address: req.query.address
+    })
+})
+
+app.get('/products',(req, res) => {
+    if(!req.query.search){
+       return res.send({
+           error: 'You must provide a search term'
+       })
+    }
+    
+    console.log(req.query.search)
+    res.send({
+        products: []
     })
 })
 
