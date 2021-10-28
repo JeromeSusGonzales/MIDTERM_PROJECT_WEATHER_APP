83fb0dda2e5cb4f444dd49b1582c8847efdb3bc6
unknown <jeromesus026@gmail.com>
2021-10-29 01:44:43 +0800

    commit

--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -1,6 +1,6 @@
 const request = require('request')
 const geocode = (address, callback)=>{
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ address +'.json?access_token=pk.eyJ1IjoiamVyb21lc3VzIiwiYSI6ImNrdjg5cjJhNTlzMzcycXE2cm9sODFmcDIifQ.GPSgWxSWKfimfyaI9f7yRQ&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token=pk.eyJ1IjoiamVyb21lc3VzIiwiYSI6ImNrdjg5cjJhNTlzMzcycXE2cm9sODFmcDIifQ.GPSgWxSWKfimfyaI9f7yRQ&limit=1'
 
     request({url: url, json: true}, (error, {body} = {}) => {
          if(error){
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -48,9 +48,9 @@ app.get('/weather', (req, res) => {
         })
     }
 
-    geocode(req.query.address, (error, {latitude, longitude, location}) => {
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
         if(error){
-            return res.send({error})
+            return res.send({ error })
         }
 
         forecast(latitude, longitude,(error, forecastData) => {
