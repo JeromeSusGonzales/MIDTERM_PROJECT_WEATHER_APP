57099ff2034c7ea2e6309ed412e6d3b0bf30a942
unknown <jeromesus026@gmail.com>
2021-10-31 23:29:00 +0800

    commit

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,23 +1,40 @@
 const request = require('request')
+
 const forecast = (latitude, longitude, callback) => {
-  const url = 'http://api.weatherstack.com/current?access_key=06258cf566e3f39de1d862a2c571cba9&query=/' + latitude + ',' + longitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=06258cf566e3f39de1d862a2c571cba9&query=/' + latitude + ',' + longitude
 
-  request({url: url, json: true}, (error, {body} = {}) => {
-      if (error){
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            // if no internet or cannot connect
+            callback('Unable to connect to the weather service', undefined)
+        } else if (body.error) {
+            // if location cannot be located (no latitude or longitude)
+            callback('Unable to find location', undefined)
+        }
 
-         callback('Unable to connect to weather service!', undefined)
+        else {
+            const temperature = body.current.temperature
+            const feelslike = body.current.feelslike
+            const weather_descriptions = body.current.weather_descriptions[0]
+            const icon = body.current.weather_icons
+            
+            var date = body.location.localtime
+            console.log(date.toLocaleString('en-US', {
+                weekday: 'long',
+                month: 'long',
+                day: 'numeric'
+            }))
+            console.log(body.location.localtime)
+            callback(undefined, {
+                date: date,
+                icon: icon,
+                degree: temperature,
+                description: weather_descriptions
+            })
 
-      }else if(body.error){
+        }
+    })
 
-        callback('Unable to find Location.', undefined)
-          
-      }else{
-
-        callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out.')
-
-      }
-
-  })
 }
 
 module.exports = forecast
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,102 +4,125 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+
+console.log(__dirname)
+
 const app = express()
-
-//Define paths or express config
+const port = process.env.PORT || 3000
+// Define paths for Express Config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath= path.join(__dirname, '../templates/partials')
 
-//Setup handlebars engine and views location
-app.set('view engine','hbs')
+// Set Handlebars engine and views location
+app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
-//Setup static Directory to serve
+// Setup Static directory to server
 app.use(express.static(publicDirectoryPath))
 
-app.get('', (req, res) => {
+
+// app.com
+app.get('', (req, res) =>{
     res.render('index',{
-        title:'Weather',
-        name: 'Jerome Sus'
+        title: 'Weather',
+        name: "Jerome G. Sus"
     })
 })
 
-app.get('/about', (req, res) => {
-    res.render('about',{
-        title:'About Me',
-        name:'Jerome Sus'
-    })
-})
-
-app.get('/help', (req, res) => {
+// app.com/help
+app.get('/help', (req, res) =>{
+    
     res.render('help',{
-        helpText:'To view the current weather forecast please provide correct location on the search box and then click the search button. Thank you!',
+        helpText: 'To view the weather forecast please provide valid address location on the search box. Thank You!',
         title: 'Help',
         name: 'Jerome Sus'
     })
 })
 
-app.get('/weather', (req, res) => {
-    if(!req.query.address){
-        return res.send({
-           error: 'You must provide an address!' 
-        })
-    }
+// app.com/about
+app.get('/about', (req, res) =>{
+    res.render('about',{
+        title: "About Me",
+        name: "Jerome Sus"
 
-    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
-        if(error){
-            return res.send({ error })
-        }
-
-        forecast(latitude, longitude,(error, forecastData) => {
-        if(error){
-            return res.send({error})
-           }
-
-           res.send({
-               forecast: forecastData,
-               location,
-               address: req.query.address
-           })
-
-        })
     })
-
 })
 
-app.get('/products',(req, res) => {
-    if(!req.query.search){
-       return res.send({
-           error: 'You must provide a search term'
-       })
+// app.com/weather
+app.get('/weather', (req, res) =>{
+    console.log("/weather")
+    if(!req.query.address){
+        return res.send({
+            error: 'Must provide an address'
+        })
     }
     
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) =>{
+        if (error){
+            return res.send({error})
+        }
+        console.log("/weather")
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error){
+                return res.send({error})
+            }
+            console.log("/weather")
+
+            res.send({
+                date: forecastData.date,
+                degree: forecastData.degree,
+                description: forecastData.description,
+                forecast: forecastData,
+                location,
+                address: req.query.address,
+                icon: forecastData.icon
+                
+
+            })
+        })
+    })
+    // res.send({
+    //     forecast: 'It is raining',
+    //     location: 'Bulan, Sorsogon',
+    //     address: req.query.address
+    // })
+})
+
+app.get('/products', (req, res) =>{
+    if (!req.query.search){
+        return res.send({
+            error: 'Must provide search term'
+        })    
+    }
+    
+
     console.log(req.query.search)
     res.send({
         products: []
     })
 })
 
-app.get('/help/*',(req, res) => {
+// /help/404
+app.get('/help/*', (req, res) =>{
     res.render('404',{
         title:'404',
-        name:'Jerome Sus',
-        errorMessage:'Help article not found.'
+        name: 'Jerome Sus',
+        errorMessage: 'Help Article not found'
     })
 })
 
-app.get('*',(req, res) => {
-    res.render('404',{
-        title:'404',
-        name:'Jerome Sus',
-        errorMessage:'Page Not Found'
-
+// 404
+app.get('*', (req, res) =>{
+    res.render("404",{
+        title: '404',
+        name: 'Jerome Sus',
+        errorMessage: 'Page not found'
     })
-
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+// to start server
+app.listen(port, () =>{
+    console.log("Server is up on port "+ port)
 })
